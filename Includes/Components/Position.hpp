873ac1355e5275650f3b30448ef75a8035200b5d
snowapril <sinjihng@gmail.com>
2021-07-23 08:45:22 +0900

    feat : add position component for represent pos in checkerboard

---
+++ Includes/Components/Position.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file Position.hpp
+ * @author snowapril (sinjihng@gmail.com)
+ * @brief Represent position component
+ * @version 0.1
+ * @date 2021-07-23
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+namespace Components
+{
+/**
+ * @brief Position coordinates in checkerboard which can be integers
+ * 
+ */
+struct Position
+{
+    int x;
+    int y;
+};
+}  // namespace Components
